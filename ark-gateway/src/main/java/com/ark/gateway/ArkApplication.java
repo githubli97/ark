6b6dc4f3f630ff3d6e645afbd51f4842aaa96768
hang.li <469995363@qq.com>
2022-11-24 22:06:32 +0800

    网关部分示例代码

---
+++ ark-gateway/src/main/java/com/ark/gateway/ArkApplication.java
@@ -0,0 +1,11 @@
+package com.ark.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArkApplication.class, args);
+	}
+}
