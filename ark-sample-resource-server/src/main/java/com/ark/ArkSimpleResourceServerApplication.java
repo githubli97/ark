fcaf4548858d7bdfca04a8b3ac4b1b86233dcb15
hang.li <hang.li@xinjifamily.com>
2022-12-29 18:55:03 +0800

    简单资源服务器

---
+++ ark-sample-resource-server/src/main/java/com/ark/ArkSimpleResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.ark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkSimpleResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkSimpleResourceServerApplication.class, args);
+	}
+
+}
---
+++ ark-sample-resource-server/src/main/java/com/ark/controller/OAuth2ResourceServerController.java
@@ -0,0 +1,22 @@
+package com.ark.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class OAuth2ResourceServerController {
+
+	@GetMapping("/message")
+	public String message() {
+		return "secret message";
+	}
+
+	@PostMapping("/message")
+	public String createMessage(@RequestBody String message) {
+		return String.format("Message was created. Content: %s", message);
+	}
+
+}
