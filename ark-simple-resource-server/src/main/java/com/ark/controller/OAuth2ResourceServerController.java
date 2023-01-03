00d98430ff2d0a2554d3ab737fec5720c2c3947f
hang.li <469995363@qq.com>
2023-01-03 17:46:45 +0800

    修改资源服务器服务名

--- ark-simple-resource-server/src/main/java/com/ark/controller/OAuth2ResourceServerController.java
+++ ark-simple-resource-server/src/main/java/com/ark/controller/OAuth2ResourceServerController.java
@@ -1,16 +1,17 @@
 package com.ark.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-
+@Slf4j
 @RestController
 public class OAuth2ResourceServerController {
 
 	@GetMapping("/message")
 	public String message() {
+		log.info("secret message");
 		return "secret message";
 	}
 
