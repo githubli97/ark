1ff254493ee0384a9f81dd4dd27def11d603b14f
hang.li <469995363@qq.com>
2023-01-05 19:22:53 +0800

    用户和mysql打通

--- ark-identify/ark-identify-adapter/src/main/java/com/ark/identify/mvc/tenant/config/AccountSignInQueryService.java
+++ ark-identify/ark-identify-adapter/src/main/java/com/ark/identify/mvc/tenant/config/AccountSignInQueryService.java
@@ -1,27 +1,27 @@
-//package com.ark.identify.mvc.tenant.config;
-//
-//import com.ark.identify.application.query.AccountQueryService;
-//import com.ark.identify.application.query.dto.AccountDTO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.authority.AuthorityUtils;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//
-//@Configuration
-//public class AccountSignInQueryService implements UserDetailsService {
-//    @Autowired
-//    private AccountQueryService accountQueryService;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        // 尝试使用手机号登录
-//        AccountDTO account = accountQueryService.getAccountByPhone(username);
-//
-//        return new User(username,
-//                account.getPassword(),
-//                AuthorityUtils.createAuthorityList(account.getAuthorityList().toArray(new String[]{})));
-//    }
-//}
+package com.ark.identify.mvc.tenant.config;
+
+import com.ark.identify.application.query.AccountQueryService;
+import com.ark.identify.application.query.dto.AccountDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+public class AccountSignInQueryService implements UserDetailsService {
+    @Autowired
+    private AccountQueryService accountQueryService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 尝试使用手机号登录
+        AccountDTO account = accountQueryService.getAccountByPhone(username);
+
+        return new User(username,
+                account.getPassword(),
+                AuthorityUtils.createAuthorityList());
+    }
+}
