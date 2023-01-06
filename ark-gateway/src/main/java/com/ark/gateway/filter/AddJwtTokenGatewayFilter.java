6c4f699615aaae3654337cd158204d3c379bffb2
hang.li <hang.li@xinjifamily.com>
2023-01-06 17:52:26 +0800

    jwt加密部分

--- ark-gateway/src/main/java/com/ark/gateway/filter/AddJwtTokenGatewayFilter.java
+++ ark-gateway/src/main/java/com/ark/gateway/filter/AddJwtTokenGatewayFilter.java
@@ -1,12 +1,21 @@
 package com.ark.gateway.filter;
 
+import com.ark.base.spring.aware.ArkApplicationContextAware;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
 
 public class AddJwtTokenGatewayFilter implements GatewayFilter, Ordered {
     public static final String JWT_TOKEN_PREFIX = "Bearer ";
@@ -16,12 +25,30 @@ public class AddJwtTokenGatewayFilter implements GatewayFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return ReactiveSecurityContextHolder.getContext().map(SecurityContext::getAuthentication)
                 .map((authentication) -> {
-                    String value = JWT_TOKEN_PREFIX + authentication.toString();
+                    String value = JWT_TOKEN_PREFIX + getJwtToken(authentication);
                     exchange.getRequest().mutate()
                             .headers((httpHeaders) -> httpHeaders.add("Authorization", value))
                             .build();
                     return chain.filter(exchange.mutate().request(exchange.getRequest()).build()).block();
                 });
+    }
+
+    private static String getJwtToken(Authentication authentication) {
+        JwtEncoder jwtEncoder = ArkApplicationContextAware.applicationContext.getBean("jwtEncoder", JwtEncoder.class);
+
+        Instant now = Instant.now();
+        long expiry = 36000L;
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 
     @Override
