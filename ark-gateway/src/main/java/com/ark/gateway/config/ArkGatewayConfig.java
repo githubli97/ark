340c2fb3b4f02e47a5e7521da5ec2789e369144c
hang.li <469995363@qq.com>
2023-01-06 17:52:26 +0800

    jwt加密部分

--- ark-gateway/src/main/java/com/ark/gateway/config/ArkGatewayConfig.java
+++ ark-gateway/src/main/java/com/ark/gateway/config/ArkGatewayConfig.java
@@ -1,45 +1,36 @@
 package com.ark.gateway.config;
 
 import com.ark.gateway.filter.AddJwtTokenGatewayFilterFactory;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.config.conditional.ConditionalOnEnabledFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 public class ArkGatewayConfig {
+    @Value("${ark.jwt.public.key}")
+    RSAPublicKey key;
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(this.key).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
     @Bean
     @ConditionalOnEnabledFilter
     public AddJwtTokenGatewayFilterFactory addJwtTokenGatewayFilterFactory() {
         return new AddJwtTokenGatewayFilterFactory();
     }
-//	@Bean
-//	public RouteLocator arkRouteLocator(RouteLocatorBuilder builder) {
-//		//@formatter:off
-//		return builder.routes()
-//				.route("path_route", r -> r.path("/message")
-//						.uri("http://ark-simple-resource"))
-//				.build();
-////				.route("host_route", r -> r.host("*.myhost.org")
-////						.uri("http://httpbin.org"))
-////				.route("rewrite_route", r -> r.host("*.rewrite.org")
-////						.filters(f -> f.rewritePath("/foo/(?<segment>.*)",
-////								"/${segment}"))
-////						.uri("http://httpbin.org"))
-//				// 熔断
-////				.route("circuitbreaker_route", r -> r.host("*.circuitbreaker.org")
-////						.filters(f -> f.circuitBreaker(c -> c.setName("slowcmd")))
-////								.uri("http://httpbin.org"))
-////				.route("circuitbreaker_fallback_route", r -> r.host("*.circuitbreakerfallback.org")
-////						.filters(f -> f.circuitBreaker(c -> c.setName("slowcmd").setFallbackUri("forward:/circuitbreakerfallback")))
-////								.uri("http://httpbin.org"))
-//				// 流量整形
-////				.route("limit_route", r -> r
-////					.host("*.limited.org").and().path("/anything/**")
-////						.filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
-////					.uri("http://httpbin.org"))
-//				// ws
-////				.route("websocket_route", r -> r.path("/echo")
-////					.uri("ws://localhost:9000"))
-//		//@formatter:on
-//	}
 }
